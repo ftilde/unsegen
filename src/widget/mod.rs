5946c019ceaa54aecc98aee5ad097b44130058b6
ftilde <ftilde@protonmail.com>
2019-02-19 22:56:56 +0100

    Add example to widget module doc

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,5 +1,52 @@
 //! Widget abstraction and some basic Widgets useful for creating basic building blocks of text
 //! user interfaces.
+//!
+//! ```no_run //tests do not provide a fully functional terminal
+//! use unsegen::base::*;
+//! use unsegen::widget::*;
+//! use unsegen::widget::widgets::*;
+//! use std::io::stdout;
+//!
+//! struct MyWidget {
+//!     layout: VerticalLayout,
+//!     prompt: PromptLine,
+//!     buffer: LogViewer,
+//! }
+//!
+//! impl Widget for MyWidget {
+//!    fn space_demand(&self) -> Demand2D {
+//!        let widgets: Vec<&Widget> = vec![&self.prompt, &self.buffer];
+//!        self.layout.space_demand(widgets.as_slice())
+//!    }
+//!    fn draw(&self, window: Window, hints: RenderingHints) {
+//!        let widgets: Vec<(&Widget, RenderingHints)> =
+//!            vec![(&self.prompt, hints), (&self.buffer, hints.active(false))];
+//!        self.layout.draw(window, widgets.as_slice());
+//!    }
+//! }
+//!
+//!
+//! fn main() {
+//!     let stdout = stdout();
+//!     let mut term = Terminal::new(stdout.lock());
+//!     let mut widget = MyWidget {
+//!         layout: VerticalLayout::new(
+//!             SeparatingStyle::AlternatingStyle(StyleModifier::new().invert(true))
+//!         ),
+//!         prompt: PromptLine::with_prompt(" > ".to_owned()),
+//!         buffer: LogViewer::new(),
+//!     };
+//!
+//!     loop {
+//!         // Put application logic here: read input, chain behavior, react to other stuff
+//!         {
+//!             let win = term.create_root_window();
+//!             widget.draw(win, RenderingHints::new().active(true));
+//!         }
+//!         term.present();
+//!     }
+//! }
+//! ```
 pub mod layouts;
 pub mod widget;
 pub mod widgets;
